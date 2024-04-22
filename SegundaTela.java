157733b89a662c06c1fe47b2d952e3829b599867
moulrras <130392211+moulrras@users.noreply.github.com>
2024-04-22 14:57:46 -0300

    Create SegundaTela.java

---
+++ SegundaTela.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class SegundaTela {
+    private JPanel panel1;
+    private JTextField txtID;
+    private JTextField txtNome;
+    private JTextField txtCpf;
+    private JTextField txtEmail;
+    private JButton btnGravar;
+    private JLabel lblID;
+    private JLabel lblNome;
+    private JLabel lblCpf;
+    private JLabel lblEmail;
+}
